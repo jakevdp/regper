8f6e4bacd02134473e9719a0a2638522b5050f8e
Jake VanderPlas <jakevdp@gmail.com>
2017-02-19 06:43:15 -0800

    Add CVX solution

---
+++ regper/cvx.py
@@ -0,0 +1,77 @@
+"""Deconvolution using CVXPY"""
+import numpy as np
+from scipy.sparse import issparse
+import cvxpy as cvx
+
+
+def least_squares(A, y, gamma_L2=0, gamma_L1=0, method=None, method_kwds=None):
+    """Iterative solution to a least squares problem
+
+    Returns argmin_x ||A*x - y||^2 + gamma_L2 ||x||^2 + gamma_L1 ||x||_1
+
+    Parameters
+    ----------
+    A : array_like or sparse
+        [N x M] projection matrix or operator
+    y : array_like
+        length-M vector
+    gamma_L2 : float (optional)
+        L2 regularization strength. Default=0
+    gamma_L1 : float (optional)
+        L1 regularization strength. Default=0
+    method : string (optional)
+        method to use (passed to ``scipy.optimize.minimize``)
+    method_kwds : dict (optional)
+        additional keywords passed to ``scipy.optimize.minimize``
+
+    Returns
+    -------
+    x : ndarray
+        length-N vector that minimizes the cost function.
+    """
+    if not issparse(A):
+        A = np.asarray(A)
+    y = np.asarray(y)
+
+    N, M = A.shape
+
+    cplx = np.issubdtype(A.dtype, complex) or np.issubdtype(y.dtype, complex)
+
+    # objective is ||Ax - y||^2 + gamma_L1 |x|_1 + gamma_L2 ||x||^2
+
+    if cplx:
+        # CVXPY does not handle complex, so we split the problem
+        Ar, Ai = A.real, A.imag
+        yr, yi = y.real, y.imag
+
+        xr = cvx.Variable(M)
+        xi = cvx.Variable(M)
+
+        error = (cvx.sum_squares(Ar*xr - Ai*xi - yr) +
+                 cvx.sum_squares(Ai*xr + Ar*xi - yi))
+        u = cvx.norm(cvx.hstack(xr, xi), 2, axis=1)
+    else:
+        x = cvx.Variable(A.shape[1])
+        error = cvx.sum_squares(A*x - y)
+        u = x
+
+    cost = error
+
+    if gamma_L1 != 0:
+        gamma_L1 = cvx.Parameter(value=gamma_L1, sign="positive")
+        cost = cost + gamma_L1 * cvx.norm(u, 1)
+    if gamma_L2 != 0:
+        gamma_L2 = cvx.Parameter(value=gamma_L2, sign='positive')
+        cost = cost + gamma_L2 * cvx.sum_squares(u)
+
+    objective = cvx.Minimize(cost)
+    prob = cvx.Problem(objective)
+    prob.solve()
+    print("Problem Status: {0}".format(prob.status))
+
+    if cplx:
+        result = np.array(xr.value).ravel() + 1j * np.array(xi.value).ravel()
+    else:
+        result = np.asarray(x.value).ravel()
+
+    return result
---
+++ regper/tests/test_cvx.py
@@ -0,0 +1,88 @@
+import numpy as np
+from ..utils import least_squares_cost
+from ..cvx import least_squares
+
+import pytest
+from numpy.testing import assert_allclose
+
+
+def data(N=15, M=10, K=None, sigma=0.1, complex=False, rseed=53489):
+    rand = np.random.RandomState(rseed)
+    A = rand.randn(N, M)
+    if K is None:
+        x = rand.randn(M)
+    else:
+        x = rand.randn(M, K)
+    if complex:
+        A = A + 1j * rand.randn(*A.shape)
+        x = x + 1j * rand.rand(*x.shape)
+    y = np.dot(A, x)
+    y += sigma * rand.randn(*y.shape)
+    if complex:
+        y += 1j * sigma * rand.randn(*y.shape)
+    return A, x, y
+
+
+def conv_data(N=15, M=10, mode='full', sigma=0.1, complex=False, rseed=53489):
+    rand = np.random.RandomState(rseed)
+    w = rand.rand(N)
+    x = rand.randn(M)
+    if complex:
+        w = w + 1j * rand.randn(N)
+        x = x + 1j * rand.rand(M)
+    y = np.convolve(w, x, mode=mode)
+    y += sigma * rand.randn(*y.shape)
+    if complex:
+        y += 1j * sigma * rand.randn(*y.shape)
+    return w, x, y
+
+
+@pytest.mark.parametrize('complex', [True, False])
+@pytest.mark.parametrize('sigma', [0, 0.001, 0.1])
+@pytest.mark.parametrize('M', [10, 15])
+@pytest.mark.parametrize('N', [15])
+def test_cvx_unregularized(N, M, sigma, complex):
+    A, x, y = data(N, M, complex=complex, sigma=sigma)
+    xfit = least_squares(A, y)
+    assert_allclose(x, xfit, atol=5 * sigma)
+
+
+@pytest.mark.parametrize('gamma_L2', [0, 1.0])
+@pytest.mark.parametrize('gamma_L1', [0, 1.0])
+@pytest.mark.parametrize('complex', [True, False])
+@pytest.mark.parametrize('sigma', [0, 0.001, 0.1])
+@pytest.mark.parametrize('M', [10, 15])
+@pytest.mark.parametrize('N', [15])
+def test_cvx_cost(N, M, sigma, complex, gamma_L1, gamma_L2):
+    rand = np.random.RandomState(543543)
+
+    A, x, y = data(N, M, complex=complex, sigma=sigma)
+    xfit = least_squares(A, y, gamma_L1=gamma_L1, gamma_L2=gamma_L2)
+    cost_0 = least_squares_cost(A, xfit, y, gamma_L1=gamma_L1,
+                                gamma_L2=gamma_L2)
+
+    # Make sure we've actually found a minimum
+    # By sampling 10 points near the fit value
+    x_perturbed = xfit[:, None] + 0.01 * rand.randn(xfit.shape[0], 10)
+    cost_perturbed = least_squares_cost(A, x_perturbed, y[:, None],
+                                        gamma_L1=gamma_L1,
+                                        gamma_L2=gamma_L2)
+
+    assert np.all(cost_0 <= cost_perturbed)
+
+
+@pytest.mark.parametrize('complex', [True, False])
+@pytest.mark.parametrize('mode', ['full', 'valid', 'same'])
+@pytest.mark.parametrize('sigma', [0])
+@pytest.mark.parametrize('M', [10, 15])
+@pytest.mark.parametrize('N', [10, 15])
+def _test_direct_deconvolution(N, M, sigma, mode, complex):
+    w, x, y = conv_data(N, M, mode=mode, complex=complex, sigma=sigma)
+    if len(y) < len(x):
+        with pytest.warns(UserWarning) as warning:
+            xfit = deconvolution(w, y, Nx=len(x), mode=mode)
+        assert len(warning) == 1
+        assert warning[0].message.args[0].startswith("Ill-posed deconvolution")
+    else:
+        xfit = deconvolution(w, y, Nx=len(x), mode=mode)
+        assert_allclose(x, xfit)
